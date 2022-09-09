7a1957f1628e0a2f45027e25cb2c12a23353850c
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-08 19:41:37 -0700

    4-puts.c task 04 commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: print "Programming is like building a multilingual puzzle using puts function"
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
