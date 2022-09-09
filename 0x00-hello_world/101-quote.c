d528b32414d94ed2bd1d80811f636b24c3933209
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-08 21:42:26 -0700

    Advanced task 101-quote.c commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
 #include <unistd.h>
-
 /**
+ * main - Entry point
  *
- * main -Entry point
- *
- *Description: print "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
- *
- * Return: Always 1 (Success)
- *
+ * Return: Always 1 [success]
  */
 
-int main(void)
+int main(void)                                    
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" 59);
-	return (1);
+char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	 write(2, str1, 59);    
+	return (1);             
 }
