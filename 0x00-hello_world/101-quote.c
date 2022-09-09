c105a7465aed33424c04f8f78d72ee6ea7edd5f2
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-08 21:12:13 -0700

    Advanced task 101-quote.c commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  *
@@ -6,7 +7,7 @@
  *
  *Description: print "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
  *
- * Return: Always 1 (Success)
+ * Return: Always 0 (Success)
  *
  */
 
