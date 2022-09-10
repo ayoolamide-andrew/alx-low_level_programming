7504072df25df8f8bdb5dd74cfe48139c7800407
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-10 05:49:46 -0700

    2-print_alphabet.c task 2 commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - To write a program that prints alphabets in lowercase.
+ *
+ * Description: "Print alphabets in lowercase."
+ *
+ * Return: Always 0 (sucess)
+ *
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = "a"; ch <= "z"; ch++)
+	{
+	putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+
+}
