62b99a4f57f91934c163e1476018bc1c6eee101b
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-11 09:14:59 -0700

    Advanced task 101-print_comb4.c  commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+/**
+ * main - program that prints possible different combinations of three digits
+ *
+ * Description: To print all possible different combinations of three digits
+ *
+ * Return: Always 0 (sucess)
+ *
+ */
+int main(void)
+{
+	int n, m, p;
+
+	for (n = 48; n <= 58; n++)
+	{
+		for (m = 49; m <= 58; m++)
+		{
+			for (p = 50; p <= 58; p++)
+			{
+				if (p > m && m > n)
+				{
+					putchar(n);
+					puthar(m);
+					putchar(p);
+					if (n != 55 || m != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
