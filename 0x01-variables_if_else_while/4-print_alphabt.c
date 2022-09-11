c4f0b9783190b6a55029120e88b61508d29f083d
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-11 01:21:42 -0700

    4-print_alphabt.c task 4 commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the alphabet in lowercase omitting q and e
+ *
+ * Description: To print alphabets in lowercase, omitting Q and E.
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	char = 'a';
+
+	while ch <= 'z';
+	{
+	if (ch != 'q' && ch != 'e')
+		{
+		putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
