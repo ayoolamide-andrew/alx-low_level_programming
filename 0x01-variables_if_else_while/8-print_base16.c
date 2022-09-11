98f8cbff5208d8235c63e3c368c3f2b8fcdcb6f3
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-11 03:13:24 -0700

    8-print_base16.c task 8 commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - A program that prints all the numbers of base 16 in lowercase
+ *
+ * Description: To print all the numbers of base 16 in lowercase
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+	int n;
+	char ch;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+
+	for (ch = 'a'; n <= 'f'; ch++)
+		putchar(ch);
+
+	putchar('\n');
+
+	return (0);
+}
