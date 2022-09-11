19985cb956a14e60f57bdc3169cd08d84fd96390
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-11 03:28:48 -0700

    8-print_base16.c task 8 commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  program that prints all possible combinations of single-digit numbers
+ *
+ * Description: To print all possible combinations of single-digit numbers
+ *
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{
+
