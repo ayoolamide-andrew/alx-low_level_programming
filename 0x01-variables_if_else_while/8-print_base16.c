05a28adbc1a431b85013c0cb82a8c3f6d99b53a2
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-11 03:38:21 -0700

    8-print_base16.c task 8 commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -11,14 +11,17 @@ int main(void)
 	int n;
 	char ch;
 
-	for (n = 0; n < 10; n++)
+
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
-	}
 
+	}
 	for (ch = 'a'; n <= 'f'; ch++)
 	{
+
 		putchar(ch);
+
 	}
 
 	putchar('\n');
