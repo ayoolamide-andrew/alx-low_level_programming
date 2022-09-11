5fd4ba0ed746802550e8b232aeddfcda00acf36e
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-11 09:21:00 -0700

    Advanced task 101-print_comb4.c commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,19 +9,19 @@
  */
 int main(void)
 {
-	int n, m, p;
+	int n, m, 1;
 
 	for (n = 48; n <= 58; n++)
 	{
 		for (m = 49; m <= 58; m++)
 		{
-			for (p = 50; p <= 58; p++)
+			for (1 = 50; 1 <= 58; 1++)
 			{
-				if (p > m && m > n)
+				if (1 > m && m > n)
 				{
 					putchar(n);
 					putchar(m);
-					putchar(p);
+					putchar(1);
 					if (n != 55 || m != 56)
 					{
 						putchar(',');
