6b1f49bfbbd8edc0b20399dcaaf2cd9223c65736
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-11 02:18:45 -0700

    6-print_numberz.c task 6 commit

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,7 +3,7 @@
 /**
  * main - A program that prints all single digit numbers of base 10.
  *
- * Description: "To print all single digits of base 10"
+ * Description: "To print all single digits of base 10 using putchar"
  *
  * Return: Always 0 (sucess)
  *
@@ -13,9 +13,9 @@ int main(void)
 	int n;
 
 	for (n = 0; n < 10; n++)
-
+	{
 		putchar(n);
-
+	}
 	putchar("\n");
 
 	return ('0');
