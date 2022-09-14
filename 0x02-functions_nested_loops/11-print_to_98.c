946b777781ac98d33206bcfd2ae7d534d29ae9db
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-14 15:45:48 -0700

    11-print_to_98.c task 11 commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_to_98 - Print all natural numbers
+ * @n: starting integer
+ *
+ * Description: print all natural nmbers from n - 98
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			printf("%d, ". n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			printf("%d, ", n);
+			n--;
+		}
+	}
+	printf("98\n");
+}
