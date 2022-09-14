c95ca584e4419c886ec2357389e54af80a2c3603
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-14 13:36:16 -0700

    2-print_alphabet_x10.c task 2 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print 10x alphabet in lowercase
+ *
+ * return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char letter = 'a';
+
+	while (round < 10)
+	{
+		letter = 'a';
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	_putchar('\n');
+
+	round++;
+	}
+}
