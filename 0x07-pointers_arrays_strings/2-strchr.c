7fea808fd66c08d49d7ccdbf8431b61c69007adb
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-09-27 18:44:28 -0700

    Task 0-memset.c commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: String to search
+ * @c: Char to find
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	while (1)
+	{
+		a = *s++;
+		if (a == c)
+		{
+			return (s - 1);
+		}
+		if (a == 0)
+		{
+			return (NULL);
+		}
+
+	}
+}
