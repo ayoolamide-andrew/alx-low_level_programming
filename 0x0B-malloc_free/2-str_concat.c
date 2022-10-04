77960af934372de2a23434c0557cbf8ffa2ae8cb
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-10-03 18:26:32 -0700

    Task 2-str_concat.c commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,7 +17,7 @@ char *str_concat(char *s1, char *s2)
 	else
 	{
 		for (i = 0; s1[i]; i++)
-			:
+			;
 	}
 
 	if (s2 == NULL)
@@ -25,7 +25,7 @@ char *str_concat(char *s1, char *s2)
 	else
 	{
 		for (j = 0; s2[j]; j++)
-			:
+			;
 	}
 
 	k = i + j + 1;
