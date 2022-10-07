08bc8e163200ba5992d35f903fe0de674307d076
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-10-06 22:11:25 -0700

    Task 3-array_range.c commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _calloc - allocates memory for an array, using malloc
+ * @nmemb: Number of elements in the array
+ * @size: Size in bytes of the array
+ * Return: void pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+	return (p);
+}
