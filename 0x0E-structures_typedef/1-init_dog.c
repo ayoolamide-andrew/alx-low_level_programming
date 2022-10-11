9676bee211d222be6067c6dac75b148271be1801
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-10-11 08:27:17 -0700

    Task dog.h commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d: struct dog
+ * name: doggo's name
+ * age: doggo's name
+ * owner: doggo's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NUL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
