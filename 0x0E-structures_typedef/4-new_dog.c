39e60063d58c80c44d2400c359c6e88c17807ef3
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-10-11 08:48:42 -0700

    Task 4-new_dog.c commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,52 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * new_dog - creates a new dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: Owner of dog
+ * Return: Pointer to new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	unsigned int nl, ol, i;
+	dog_t *dog;
+
+	if (name == NULL || owner == NULL)
+		return  (NULL);
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+
+	for (nl = 0; name[nl]; nl++)
+		;
+	n1++;
+	dog->name = malloc(n1 * sizeof(char));
+
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < n1; i++)
+		dog->name[i] = name[i];
+
+	dog->age = age;
+	for (ol = 0; owner[ol]; ol++)
+		;
+	o1++;
+
+	dog->owner = malloc(ol * sizeof(char));
+
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0, i < o1; i++)
+		dog->owner[i] = owner[i];
+
+	return (dog);
+}
