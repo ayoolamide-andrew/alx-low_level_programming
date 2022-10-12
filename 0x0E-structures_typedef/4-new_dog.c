f6f8bfdc2f98f1ec7c6e1306532288b4c12c8b59
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-10-11 19:25:39 -0700

    Task 4-new_dog.c commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -28,6 +28,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; name[i] != '\0'; i++)
 		new_dog->name[i] = name[i];
+	new_dog->name[i] = '\0';
 
 	new_dog->age = age;
 
@@ -40,6 +41,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	}
 	for (i = 0; owner[i] != '\0'; i++)
 		new_dog->owner[i] = owner[i];
+	new_dog->owner[i] = '\0';
 
 	return (new_dog);
 }
