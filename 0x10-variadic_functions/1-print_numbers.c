c4ad053917ab925fe5f5508ea8abeaede0aa3de5
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-10-13 17:38:35 -0700

    Task 1-print_numbers.c commit

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,25 +1,25 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers given as parameters
- * @separators: String to be printed between numbers
+ * print_numbers - prints numbers
+ * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  * Return: No return
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	unsigned int i;
 	va_list list;
+	unsigned int i;
 
 	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-			printf("%d", va_arg(list, int));
-			if (separator && i < n -1)
-				printf("%s", separator);
+		printf("%d", va_arg(valist, int));
+		if (separator && i < n - 1)
+			printf("%s", separator);
 	}
-	
+
 	printf("\n");
 	va_end(list);
 }
