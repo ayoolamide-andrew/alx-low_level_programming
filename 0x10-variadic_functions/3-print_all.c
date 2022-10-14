f3f479077b5434c56c787f0363e73ccc26b04349
Ayoolamide-andrew <Ayoolamide_andrew@yahoo.com>
2022-10-13 18:10:38 -0700

    Task 3-print_all.c commit

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -24,7 +24,7 @@ void print_all(const char * const format, ...)
 				break;
 			} j++;
 		}
-		switch format[i]
+		switch (format[i])
 		{
 			case 'c':
 				printf("%c", va_arg(valist, int)), c = 1;
